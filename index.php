cea7ebef9e76d2193b684dc30e6314e4ab67b6dd
mathieucaces <mathieu.caces@hetic.net>
2018-03-18 13:30:29 +0100

    index, reset css

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
+<?php
+
+
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- reset csss -->
+    <link rel="stylesheet" href="styles/reset.min.css">
+    <!-- main stylesheet -->
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Meteo Direct</title>
 </head>
 <body>
-  
+
+    <header>
+
+    </header>
+
+    <section>
+
+    </section>
+
+    <footer>
+
+    </footer>
+
 </body>
 </html>
