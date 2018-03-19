50bf98df00bf86b4b62266537abef8c41758e6fd
mathieucaces <mathieu.caces@hetic.net>
2018-03-19 14:34:08 +0100

    start css of index, import fonts

--- index.php
+++ index.php
@@ -1,19 +1,13 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- reset csss -->
-    <link rel="stylesheet" href="styles/reset.min.css">
+    <!-- reset css -->
+    <link rel="stylesheet" href="src/stylesheets/reset.css">
     <!-- main stylesheet -->
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="src/stylesheets/style.css">
     <title>Meteo Direct</title>
 </head>
 <body>
