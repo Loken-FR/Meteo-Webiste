b4b18d1486c57bd2710b77b063f9651f7eb11bc2
mathieucaces <mathieu.caces@hetic.net>
2018-03-23 16:27:24 +0100

    add meteo section, change responsive, add footer

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- reset css -->
-    <link rel="stylesheet" href="src/stylesheets/reset.css">
+    <link rel="stylesheet" href="src/styles/reset.css">
     <!-- main stylesheet -->
-    <link rel="stylesheet" href="src/stylesheets/style.css">
+    <link rel="stylesheet" href="src/styles/style.css">
     <title>Meteo Direct</title>
 </head>
 <body>
@@ -20,7 +20,7 @@
             </div>
             <div class="search-city">
                 <form action="#" method="GET">
-                <input class="input-text" type="text" value="test" name="city">
+                <input class="input-text" type="text" name="city">
                 <input class="input-submit" type="submit">
             </div>
             <div class="geolocation">
@@ -31,7 +31,7 @@
             <ul class="menu">
                 <li class="text text-1"><a href="#">Accueil</a></li>
                 <li class="text text-2"><a href="#">Prévisions</a></li>
-                <li class="text text-3"><a href="#">Observations</a></li>
+                <li class="text text-3"><a href="#">Actualités</a></li>
             </ul>
         </div>
     </form>
@@ -42,38 +42,77 @@
         <div class="contents-section">
             <!-- header contents -->
             <div class="header-contents">
-                <h2 class="title">Meteo France</h2>
+                <h2 class="title">METEO FRANCE</h2>
+                <div class="separator">-</div>
                 <div class="text">Aujourd'hui</div>
             </div>
             <!-- main contents -->
             <div class="main-contents">
-            <div class="primary-city">
-                <div class="city-1">
-                    <h3 class="city-name"></h3>
-                    <div class="meteo-section">
+                <div class="primary-city">
+                    <div class="city city-1">
+                        <h3 class="city-name">Paris</h3>
+                        <div class="map"></div>
+                        <div class="meteo-section">
+                            <div class="morning">
+                                <h4 class="time-day">Matin :</h4>
+                                <div class="sky">Nuageux</div>
+                                <div class="temperature">3°C</div>
+                            </div>
+                            <div class="afternoon">
+                                <h4 class="time-day">Après midi :</h4>
+                                <div class="sky">Nuageux</div>
+                                <div class="temperature">9°C</div>
+                            </div>
+                            <div class="evening">
+                                <h4 class="time-day">Soir :</h4>
+                                <div class="sky">Pluvieux</div>
+                                <div class="temperature">2°C</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="city city-2">
+                        <h3 class="city-name">Marseille</h3>
+                        <div class="map"></div>
+                        <div class="meteo-section">
+                            <div class="morning">
+                                <h4 class="time-day">Matin :</h4>
+                                <div class="sky">Ensoleillé</div>
+                                <div class="temperature">7°C</div>
+                            </div>
+                            <div class="afternoon">
+                                <h4 class="time-day">Après midi :</h4>
+                                <div class="sky">Ensoleillé</div>
+                                <div class="temperature">15°C</div>
+                            </div>
+                            <div class="evening">
+                                <h4 class="time-day">Soir :</h4>
+                                <div class="sky">Eclaircie</div>
+                                <div class="temperature">6°C</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="city city-3">
+                        <h3 class="city-name">Lyon</h3>
+                        <div class="map"></div>
+                        <div class="meteo-section">
+                            <div class="morning">
+                                <h4 class="time-day">Matin :</h4>
+                                <div class="sky">Nuageux</div>
+                                <div class="temperature">4°C</div>
+                            </div>
+                            <div class="afternoon">
+                                <h4 class="time-day">Après midi :</h4>
+                                <div class="sky">Ensoleillé</div>
+                                <div class="temperature">10°C</div>
+                            </div>
+                            <div class="evening">
+                                <h4 class="time-day">Soir :</h4>
+                                <div class="sky">Eclaircie</div>
+                                <div class="temperature">2°C</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="city-2">
-                    <h3 class="city-name"></h3>
-                    <div class="meteo-section">
-                    </div>
-                </div>
-                <div class="city-3">
-                    <h3 class="city-name"></h3>
-                    <div class="meteo-section">
-                    </div>
-                </div>
-                <div class="city-4">
-                    <h3 class="city-name"></h3>
-                    <div class="meteo-section">
-                    </div>
-                </div>
-                <div class="city-5">
-                    <h3 class="city-name"></h3>
-                    <div class="meteo-section">
-                    </div>
-                </div>
-            </div>
             </div>
         </div>
     </section>
@@ -83,11 +122,11 @@
         <div class="menu-section">
             <ul class="menu">
                 <li class="text text-1"><a href="#">A propos</a></li>
-                <li class="text text-2"><a href="#">Contact</a></li>
+                <li class="text text-2"><a href="#">Nous Contacter</a></li>
                 <li class="text text-3"><a href="#">Mentions légales</a></li>
             </ul>
         </div>
     </footer>
-    <script src="scripts/script.js" type="text/javascript"></script>
+    <!-- <script src="scripts/script.js" type="text/javascript"></script> -->
 </body>
 </html>
